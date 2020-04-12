0230d17f4803d9d8f95342841f6fceb11bcc7ead
Muhammad Harisuddin Thohir <me@harisuddin.com>
2020-04-13 06:53:28 +0700

    change logger

--- lib/log.dart
+++ lib/log.dart
@@ -1,48 +1,96 @@
-import 'cf.dart';
+
+
+import 'package:cresenity/constant/config.dart';
+
 import 'logger.dart';
-import 'constant/config.dart' as config;
 
-
-/**
- * @author Muhammad Harisuddin Thohir <me@harisuddin.com>
- * @since 2020-02-17 20:43
- * @license Ittron Global Teknologi <ittron.co.id>
- */
+/// @author Muhammad Harisuddin Thohir <me@harisuddin.com>
+/// @since 2020-01-21 11:19
+/// @license Ittron Global Teknologi <ittron.co.id>
 
 class Log{
-  Log(dynamic message){
-    if(!config.isProduction || config.enableForceLogger){
-      CF.logger().log(Level.debug, message);
+  static bool enableForceLog = true;
+  static bool _enableLog = !AppConfig.isProduction || enableForceLog;
+  static bool hasTitle = false;
+  static int lineLength = 128;
+
+  static Logger _logger = Logger(
+    printer: PrettyPrinter(
+      methodCount: 0, // number of method calls to be displayed
+      errorMethodCount: 8, // number of method calls if stacktrace is provided
+      lineLength: lineLength, // width of the output
+      colors: false, // Colorful log messages
+      printEmojis: true, // Print an emoji for each log message
+      printTime: false, // Should each log print contain a timestamp),
+    ),
+    output: _ConsoleOutput(),
+  );
+
+  Log(dynamic message, [String title]){
+    v(message, title);
+  }
+
+  static d(dynamic message, [String title]){
+    if(_enableLog){
+      _log(message, Level.debug, title);
     }
   }
 
-  static d(dynamic message){
-    if(!config.isProduction || config.enableForceLogger) {
-      CF.logger().d(message);
+  static i(dynamic message, [String title]){
+    if(_enableLog){
+      _log(message, Level.info, title);
     }
   }
 
-  static e(dynamic message){
-    if(!config.isProduction || config.enableForceLogger) {
-      CF.logger().log(Level.error, message);
+  static v(dynamic message, [String title]){
+    if(_enableLog){
+      _log(message, Level.verbose, title);
     }
   }
 
-  static i(dynamic message){
-    if(!config.isProduction || config.enableForceLogger) {
-      CF.logger().log(Level.info, message);
+  static w(dynamic message, [String title]){
+    if(_enableLog){
+      _log(message, Level.warning, title);
     }
   }
 
-  static v(dynamic message){
-    if(!config.isProduction || config.enableForceLogger) {
-      CF.logger().log(Level.verbose, message);
+  static e(dynamic message, [String title]){
+    if(_enableLog){
+      _log(message, Level.error, title);
     }
   }
 
-  static w(dynamic message){
-    if(!config.isProduction || config.enableForceLogger) {
-      CF.logger().log(Level.warning, message);
+  static wtf(dynamic message, [String title]){
+    if(_enableLog){
+      _log(message, Level.wtf, title);
+    }
+  }
+
+  static _log(dynamic message, [Level level, String title]){
+    if(title != null){
+      print('┌${'─'*(lineLength-1)}');
+      for(String val in title.split('\n')){
+        print('│ $val');
+      }
+      hasTitle = true;
+    }else{
+      hasTitle = false;
+    }
+    if(level == null){
+      _logger.d(message);
+    }else{
+      _logger.log(level, message);
+    }
+  }
+
+}
+
+class _ConsoleOutput extends LogOutput {
+  @override
+  void output(OutputEvent event) {
+    for (var line in event.lines) {
+      if(Log.hasTitle) line = line.replaceFirst('┌', '├');
+      print(line);
     }
   }
 }
