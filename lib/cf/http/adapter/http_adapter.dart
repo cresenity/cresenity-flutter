be816335f1c2fee5c952cf83ace0babf5f5abc0e
Suhardik Nirmansyah <suhardik@ittron.id>
2020-05-27 17:40:12 +0700

    update http adapter

--- lib/cf/http/adapter/http_adapter.dart
+++ lib/cf/http/adapter/http_adapter.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:io';
 
 import 'package:async/async.dart';
@@ -13,43 +11,37 @@ import '../request.dart';
 import '../response.dart';
 import 'package:path/path.dart' as path;
 
-
 class HttpAdapter extends Adapter {
-
-
   Future<Response> request(Request request) async {
-
     var httpRequest = http.MultipartRequest(request.method.toUpperCase(), Uri.parse(request.url));
 
-    request.paramCollection.asPostStringMap().forEach((key,value) {
-      httpRequest.fields[key]=value;
+    request.paramCollection.asPostStringMap().forEach((key, value) {
+      httpRequest.fields[key] = value;
     });
 
-    request.fileCollection.forEach((key,value) {
-      if (!value is File) {
-        return null;
+    request.fileCollection.forEach((key, value) async {
+      if (value != null) {
+        // print('SN_LOG + IS FILE + ' + (value is File).toString());
+        if (value is File) {
+          // print('SN_LOG + IS EXIST + ' + value.existsSync().toString());
+          if (value.existsSync()) {
+//            String filePath = value.path;
+//            String basename = path.basename(filePath);
+//            var stream = new http.ByteStream(DelegatingStream.typed(value.openRead()));
+//            var length = value.lengthSync();
+//            http.MultipartFile multipartFile = new http.MultipartFile("$key", stream, length, filename: basename);
+            http.MultipartFile multipartFile = await http.MultipartFile.fromPath('$key', value.path);
+            httpRequest.files.add(multipartFile);
+          }
+        }
       }
-      File file = value as File;
-      if (file.existsSync()) {
-        String filePath = file.path;
-
-        String basename = path.basename(filePath);
-        var stream = new http.ByteStream(
-            DelegatingStream.typed(file.openRead()));
-        var length = file.lengthSync();
-        http.MultipartFile multipartFile =
-        new http.MultipartFile("$key", stream, length, filename: basename);
-        httpRequest.files.add(multipartFile);
-      }
-
     });
 
     var httpResponse = await httpRequest.send();
     String responseBody = await httpResponse.stream.bytesToString();
 
-    Response response = Response(body: responseBody, statusCode:httpResponse.statusCode);
+    Response response = Response(body: responseBody, statusCode: httpResponse.statusCode);
 
     return response;
-
   }
 }
